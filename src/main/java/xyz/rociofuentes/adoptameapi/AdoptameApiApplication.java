47afa3f41a55d49007add035b0917efb188eda35
Rocio Fuentes <rocio.fc.511@gmail.com>
2022-04-16 23:57:46 -0500

    Initial commit.

---
+++ src/main/java/xyz/rociofuentes/adoptameapi/AdoptameApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.rociofuentes.adoptameapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptameApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptameApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/rociofuentes/adoptameapi/AdoptameApiApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.rociofuentes.adoptameapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptameApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
