e5fd2e0cced24c78e7e4aa8be3b7f2fbcf8c9152
Rocio Fuentes <rocio.fc.511@gmail.com>
2022-04-25 22:16:13 -0500

    Fix the database script.

--- src/main/resources/bd/initial_load.sql
+++ src/main/resources/bd/initial_load.sql
@@ -2,12 +2,12 @@ INSERT INTO rol (nombre)
 VALUES ('administrador'), ('albergue');
 
 INSERT INTO usuario(correo, password_hash, activo, fecha_alta, id_rol)
-VALUES ('mila.martz@opa.org', '$2b$10$zJ2CDAFElHKQnWM7cbWQaOwuNARqQw8Jd7898fzChulbow32d/ody', true, date('2015-12-17'), 1),
+VALUES ('sociedad@opa.org', '$2b$10$zJ2CDAFElHKQnWM7cbWQaOwuNARqQw8Jd7898fzChulbow32d/ody', true, date('2015-12-17'), 1),
        ('frucevolucion12@gmail.com', '$2b$10$S0OQjgLoCM1RX6nBe3oOJOmFbRcY.tzgjdY4PSfLV.MA6FUX38cDy', false, date('2021-11-02'), 2),
        ('a.c.proanimal@gmail.com', '$2b$10$nIqon8bT5E2YwRntvUVWAe6BPcZkA6wUrdOBfhnJVKCPplJuU55ci', true, date('2021-12-01'), 2);
 
 INSERT INTO albergue(nombre, direccion, contacto, telefono, correo)
-VALUES ('Sociedad por los Animalitos', 'Calle 11B Col Los Girasoles Mérida Yucatán', 'Mila Martinez', '999 5678243', 'mila.martz@opa.org' ),
+VALUES ('Sociedad por los Animalitos', 'Calle 11B Col Los Girasoles Mérida Yucatán', 'Mila Martinez', '999 5678243', 'sociedad@opa.org' ),
        ('Evolución Animal', 'Merida-Uman, Fracc. San Lorenzo', 'Francisco Uc', '999 1930628', 'frucevolucion12@gmail.com'),
        ('Acción pro animal AC', 'Calle 46 No. 400 x 21 Fraccionamiento Pacabtun', 'Gilberto Vaquerano', '999 7487079','a.c.proanimal@gmail.com');
 
@@ -15,3 +15,4 @@ INSERT INTO mascota(nombre, tipo_mascota, sexo, fecha_nacimiento, raza, descripc
 VALUES ('Gato', 'Gato', 'Macho', date('2015-03-27'), 'Domestico pelo medio', 'Es un gato de tamaño gande, muy juguetón, le gustan mucho los treats y toma muchas siestas durante el día, su ronroneo es muy bajito.', true, date('2022-04-19'), false, 3),
        ('Mercurio', 'Perro', 'Macho', date('2007-09-09'), 'Mestizo', 'Es un perro juguetón, de pelo corto, tamaño mediano y le gusta comer carne de pollo.', false, date('2017-03-03'), false, 2),
        ('Kiki', 'Gato', 'Hembra', date('2018-06-27'), 'Gato doméstico, pelo largo', 'Es una gatita muy tierna, le gusta correr, su ronroneo es muy fuerte que parece una olla de tamales hirviendo, su maullido es muy pequeño pero cuando tiene hambre puede sonar mucho más.', true, date('2018-01-02'), false, 1);
+
--- src/main/resources/bd/schema.sql
+++ src/main/resources/bd/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE adoptame;
 
 CREATE TABLE rol(
-    id_rol int AUTO_INCREMENT,
+    id_rol bigint AUTO_INCREMENT,
     nombre VARCHAR(120),
     PRIMARY KEY (id_rol)
 );
@@ -11,23 +11,23 @@ CREATE TABLE usuario(
     password_hash VARCHAR(100) NOT NULL,
     activo BOOLEAN,
     fecha_alta date,
-    id_rol int,
+    id_rol bigint,
     PRIMARY KEY (correo),
     FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
 );
 
 CREATE TABLE albergue(
-    id_albergue int AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) UNIQUE NOT NULL,
+    id_albergue bigint AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
     direccion VARCHAR(500) NOT NULL,
     contacto VARCHAR(100) NOT NULL,
     telefono VARCHAR(13) NOT NULL,
-    correo VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
     FOREIGN KEY (correo) REFERENCES usuario(correo)
 );
 
 CREATE TABLE mascota(
-    id_mascota INT NOT NULL AUTO_INCREMENT,
+    id_mascota bigint NOT NULL AUTO_INCREMENT,
     tipo_mascota VARCHAR(20),
     nombre VARCHAR(100),
     sexo VARCHAR(20) NOT NULL ,
@@ -37,7 +37,7 @@ CREATE TABLE mascota(
     castrado BOOLEAN,
     fecha_registro DATE,
     disponible BOOLEAN,
-    id_albergue INT NOT NULL,
+    id_albergue bigint NOT NULL,
     PRIMARY KEY (id_mascota),
     FOREIGN KEY (id_albergue) REFERENCES albergue (id_albergue)
 );
